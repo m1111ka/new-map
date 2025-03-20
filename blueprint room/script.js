e91d7cfc47ad06b4555a647a60c72e7e66d1d122
m1111ka <mikatse0@gmail.com>
2025-03-20 14:14:45 +0000

    Add files via upload

---
+++ blueprint room/script.js
@@ -0,0 +1,44 @@
+
+console.log()
+
+$(document).ready(function(){
+$(".marker-6").click(function(){
+$(".book").toggle();
+});
+});
+
+$(document).ready(function(){
+    $(".marker-1").click(function(){
+    $(".bench").toggle();
+    });
+    });
+
+$(document).ready(function(){
+    $(".marker-2").click(function(){
+    $(".energy").toggle();
+    });
+    });
+
+$(document).ready(function(){
+        $(".marker-3").click(function(){
+        $(".map").toggle();
+        });
+    });
+
+$(document).ready(function(){
+        $(".marker-4").click(function(){
+        $(".window").toggle();
+        });
+    });
+
+$(document).ready(function(){
+        $(".marker-5").click(function(){
+        $(".power").toggle();
+        });
+    });
+
+$(document).ready(function(){
+        $(".marker-7").click(function(){
+        $(".screen").toggle();
+        });
+    });
